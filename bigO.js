ffd33fe256441ef7934163899e25ec6ea727b0c5
abdullahlaeeque <60699221+abdullahlaeeque@users.noreply.github.com>
2021-07-15 17:43:43 +0530

    Update bigO.js
    changes

--- bigO.js
+++ bigO.js
@@ -1,11 +1,14 @@
-function pythagoreanTriplet(n){            
-    for(var a = 1; a < n - 1; a++){
+function pythagoreanTriplet(n){   
+    
+ for(var a = 1; a < n - 1; a++){
       for(var b = a; b < n; b++){
         c = Math.sqrt(a * a + b * b);
         if(c % 1 === 0){
-          console.log(a, b, c);
+ 
+ console.log(a, b, c);
         }
       }
     }
   }
-  pythagoreanTriplet(9007199254740991)
+
+pythagoreanTriplet(9007199254740991)
