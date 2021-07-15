5815d0c44b18acd24b1fcbde68cebb839b71aee2
abdullahlaeeque <60699221+abdullahlaeeque@users.noreply.github.com>
2021-07-15 17:49:34 +0530

    Update bigO.js
    updates

--- bigO.js
+++ bigO.js
@@ -1,14 +1,14 @@
-function pythagoreanTriplet(n){   
+function pythagoreanTriplet(n){   //funtion
     
- for(var a = 1; a < n - 1; a++){
+ for(var a = 1; a < n - 1; a++){  //logic to find the triplets of the given value
       for(var b = a; b < n; b++){
         c = Math.sqrt(a * a + b * b);
         if(c % 1 === 0){
  
- console.log(a, b, c);
+ console.log(a, b, c); //log to show the values the program
         }
       }
     }
   }
 
-pythagoreanTriplet(9007199254740991)
+pythagoreanTriplet(9007199254740991) //value of MAX_SAFE_INTEGER to find the trtiplets between the value 
